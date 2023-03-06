5a3c1eae80f4e7d9de8e08bd8b9178f9afa62da0
Rana Hamid <rhamid@mymedicalhub.com>
2023-03-06 21:00:15 +0600

    IBaseService

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services
+{
+    public class BaseService:IBaseService
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseDto ResponseModel { get; set; }
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

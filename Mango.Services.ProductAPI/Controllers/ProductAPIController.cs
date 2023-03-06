efbbae4085f100c7d15dccff41f3f7a77b4d5db4
Rana Hamid <rhamid@mymedicalhub.com>
2023-03-06 20:57:46 +0600

    ApiRequest

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -15,7 +15,7 @@ namespace Mango.Services.ProductAPI.Controllers
         public ProductAPIController(IProductRepository productRepository)
         {
             _productRepository = productRepository;
-            this._response= new ResponseDto();
+            this._response = new ResponseDto();
         }
 
         [HttpGet]
@@ -24,7 +24,7 @@ namespace Mango.Services.ProductAPI.Controllers
             try
             {
                 var products = await _productRepository.GetProducts();
-                _response.Result=products;
+                _response.Result = products;
             }
             catch (Exception ex)
             {
@@ -50,5 +50,54 @@ namespace Mango.Services.ProductAPI.Controllers
             }
             return _response;
         }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var product = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var product = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                //var product = await _productRepository.GetProductById(id);
+                bool isSuccess = await _productRepository.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
     }
 }
---
+++ Mango.Web/Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services
+{
+    public class ProductService:IProductService
+    {
+        public Task<T> GetAllProductAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllProductByIdAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> CreateProductAsync<T>(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateProductAsync<T>(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteProductAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

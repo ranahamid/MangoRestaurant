ae1e653e8801a82bf10647f8d86918215833ab00
RanaHamid <ranahamid007@gmail.com>
2023-04-10 18:16:16 +0600

    .

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -52,20 +52,8 @@ namespace Mango.Web.Controllers
                 return View(cartDto);
             }
         }
-        //[HttpPost]
-        //public async Task<IActionResult> Checkout(CartDto cartDto)
-        //{
-        //    try
-        //    {
-        //        var accessToken = await HttpContext.GetTokenAsync("access_token");
-        //        var response = await _cartService.Checkout<ResponseDto>(cartDto.CartHeader, accessToken);
-        //        return RedirectToAction(nameof(Confirmation));
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        return View(cartDto);
-        //    }
-        //}
+     
+
         public async Task<IActionResult> Confirmation()
         {
             return View();

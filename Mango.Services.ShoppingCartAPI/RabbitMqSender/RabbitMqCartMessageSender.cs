717998c67af539e246300e392d21fd15780451dd
Rana Hamid <rhamid@mymedicalhub.com>
2023-04-16 19:31:53 +0600

    RabbitMqCartMessageSender

--- Mango.Services.ShoppingCartAPI/RabbitMqSender/RabbitMqCartMessageSender.cs
+++ Mango.Services.ShoppingCartAPI/RabbitMqSender/RabbitMqCartMessageSender.cs
@@ -1,4 +1,5 @@
 ﻿using Mango.MessageBus;
+using RabbitMQ.Client;
 
 namespace Mango.Services.ShoppingCartAPI.RabbitMqSender
 {
@@ -7,9 +8,24 @@ namespace Mango.Services.ShoppingCartAPI.RabbitMqSender
         private readonly string _hostName;
         private readonly string _password;
         private readonly string _userName;
+        public RabbitMqCartMessageSender()
+        {
+            _hostName = "localhost";
+            _password = "guest";
+            _userName = "guest";
+        }
+
         public void SendMessage(BaseMessage message, string queueName)
         {
-            throw new NotImplementedException();
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostName,
+                Password = _password,
+                UserName = _userName,
+            };
+            var _connection = factory.CreateConnection();
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
         }
     }
 }

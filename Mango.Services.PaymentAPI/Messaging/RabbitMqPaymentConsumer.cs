6e36a0f4366b9d2361eca5252c806326dcd1844c
Rana Hamid <rhamid@mymedicalhub.com>
2023-04-16 22:04:34 +0600

    ExchangeName

---
+++ Mango.Services.PaymentAPI/Messaging/RabbitMqPaymentConsumer.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using Mango.MessageBus;
+using Mango.Services.PaymentAPI.Messages;
+using Mango.Services.PaymentAPI.RabbitMqSender;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using PaymentProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Mango.Services.PaymentAPI.Messaging
+{
+    public class RabbitMqPaymentConsumer : BackgroundService
+    {
+
+        private IConnection _connection;
+        private IModel _channel; 
+        private readonly IConfiguration _configuration; 
+        private IMapper _mapper;
+        private readonly string orderPaymentProcessTopic;
+        private readonly string orderUpdatePaymentProcessTopic;
+
+
+        private readonly IProcessPayment _processPayment;
+        private readonly IRabbitMqPaymentMessageSender _rabbitMqPaymentMessageSender;
+        public RabbitMqPaymentConsumer(IProcessPayment processPayment, IConfiguration configuration ,IMapper mapper,
+            IRabbitMqPaymentMessageSender rabbitMqPaymentMessageSender)
+        {
+            _processPayment = processPayment;
+            _configuration = configuration; 
+            orderPaymentProcessTopic = _configuration.GetValue<string>("OrderPaymentProcessTopic");
+            orderUpdatePaymentProcessTopic = _configuration.GetValue<string>("OrderUpdatePaymentProcessTopic");
+
+            _mapper = mapper;
+            _rabbitMqPaymentMessageSender = rabbitMqPaymentMessageSender;
+
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+
+            _channel.QueueDeclare(queue: orderPaymentProcessTopic,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                PaymentRequestMessage paymentRequest = JsonConvert.DeserializeObject<PaymentRequestMessage>(content);
+               
+
+
+                HandleMessage(paymentRequest).GetAwaiter().GetResult();
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+            _channel.BasicConsume(queue: orderPaymentProcessTopic, autoAck:false,consumer: consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessage(PaymentRequestMessage paymentRequest)
+        {
+            var result = _processPayment.PaymentProcessor();
+            UpdatePaymentResultMessage updatePaymentResultMessage = new UpdatePaymentResultMessage
+            {
+                Status = result,
+                OrderId = paymentRequest.OrderId,
+                Email = paymentRequest.Email,
+            };
+            try
+            {
+                //Azure
+                //await _messageBus.PublishMessage(paymentRequest, orderPaymentProcessTopic);
+                //await args.CompleteMessageAsync(args.Message);
+
+                //RabbitMQ
+                _rabbitMqPaymentMessageSender.SendMessage(updatePaymentResultMessage, orderUpdatePaymentProcessTopic);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Mango.Services.PaymentAPI/RabbitMqSender/RabbitMqPaymentMessageSender.cs
@@ -0,0 +1,79 @@
+﻿using Mango.MessageBus;
+ 
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Mango.Services.PaymentAPI.RabbitMqSender
+{
+    public class RabbitMqPaymentMessageSender : IRabbitMqPaymentMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _password;
+        private readonly string _userName;
+
+        private const string ExchangeName = "PublishSubscripePaymentUpdate_Exchange";
+        private IConnection _connection;
+        public RabbitMqPaymentMessageSender()
+        {
+            _hostName = "localhost";
+            _password = "guest";
+            _userName = "guest";
+        }
+
+        public void SendMessage(BaseMessage message, string queueName)
+        {
+            try
+            {
+                if (ConnectionExists())
+                {
+                    using var channel = _connection.CreateModel();
+
+                    channel.QueueDeclare(queue: queueName,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Fanout, durable: false);
+
+                    var json = JsonConvert.SerializeObject(message);
+                    var body = Encoding.UTF8.GetBytes(json);
+
+                    channel.BasicPublish(exchange: ExchangeName,
+                                         routingKey: queueName,
+                                         basicProperties: null,
+                                         body: body);
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory { HostName = "localhost" };
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private bool ConnectionExists()
+        {
+            if (_connection != null)
+            {
+                return true;
+            }
+            CreateConnection();
+            return _connection != null;
+        }
+    }
+}

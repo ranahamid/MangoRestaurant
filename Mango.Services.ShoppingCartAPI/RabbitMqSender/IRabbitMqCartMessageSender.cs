0d365e28cd7b2b5a429a80b0688499104c599b35
Rana Hamid <rhamid@mymedicalhub.com>
2023-04-16 19:16:52 +0600

    NotImplementedException

---
+++ Mango.Services.ShoppingCartAPI/RabbitMqSender/IRabbitMqCartMessageSender.cs
@@ -0,0 +1,9 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMqSender
+{
+    public interface IRabbitMqCartMessageSender
+    {
+        void SendMessage(BaseMessage message, string queueName);
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/RabbitMqSender/RabbitMqCartMessageSender.cs
@@ -0,0 +1,15 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMqSender
+{
+    public class RabbitMqCartMessageSender : IRabbitMqCartMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _password;
+        private readonly string _userName;
+        public void SendMessage(BaseMessage message, string queueName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
